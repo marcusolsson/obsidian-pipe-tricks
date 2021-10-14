2f32e7edb6fed1f3ee956290c84a54164528e941
Marcus Olsson <marcus.olsson@hey.com>
2021-10-14 09:35:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Pipe tricks for Obsidian
+
+An Obsidian that adds [pipe tricks](https://en.wikipedia.org/wiki/Help:Pipe_trick) to the Wikilink format used by Obsidian.
+
+From Wikipedia:
+
+> The **pipe trick** uses the [pipe character](https://en.wikipedia.org/wiki/Pipe_character "Pipe character") ("`|`") to save typing the label of a [piped link](https://en.wikipedia.org/wiki/Help:Piped_link "Help:Piped link") for several kinds of [wiki links](https://en.wikipedia.org/wiki/Wikilink "Wikilink"). This can avoid potentially making an error while typing the label.
+>
+> When the last character of a link is the pipe character, the pipe trick will automatically generate the text that displays for that link. Its processing removes the parenthesized part of the link title, handles commas in the title, and removes namespace prefixes. The described processing happens at the point of saving the page data, and the generated text is saved with the rest of the page's wiki markup – thus, it is merely an aid to editing.
+>
+> An even better way to save keystrokes that doesn't need any additional characters is by simply attaching text to the link, as in `[[train]]s`.
+
+## Known limitations
+
+- The plugin doesn't remove namespace prefixes. Mostly because I haven't seen the need for it yet. Let me know if you'd find it useful.
+
+## Examples from Wikipedia
+
+These examples are also used as test cases:
+
+- [[Pipe (computing)|]] -> [[Pipe (computing)|Pipe]]
+- [[Phoenix, Arizona|]] -> [[Phoenix, Arizona|Phoenix]]
+- [[pipe (computing)|]]s -> [[pipe (computing)|pipes]]
+- [[pipe (computing)|]]mill -> [[pipe (computing)|pipemill]]
+- [[Yours, Mine and Ours (1968 film)|]] -> [[Yours, Mine and Ours (1968 film)|Yours, Mine and Ours]]
+- [[Il Buono, il Brutto, il Cattivo|]] -> [[Il Buono, il Brutto, il Cattivo|Il Buono]]
+- [[pipe (computing)|]]! -> [[pipe (computing)|pipe]]!
---
+++ main.ts
@@ -0,0 +1,93 @@
+import { Plugin } from "obsidian";
+
+export default class ExamplePlugin extends Plugin {
+  async onload() {
+    this.registerMarkdownPostProcessor((element) => {
+      getInternalLinks(element).forEach(updateLinkText);
+    });
+  }
+}
+
+// getInternalLinks returns all links generated from the [[double bracket syntax]].
+export function getInternalLinks(element: HTMLElement) {
+  const links = element.querySelectorAll("a");
+
+  const internalLinks = [];
+  for (let index = 0; index < links.length; index++) {
+    const link = links.item(index);
+
+    if (link.hasClass("internal-link")) {
+      internalLinks.push(link);
+    }
+  }
+
+  return internalLinks;
+}
+
+// updateLinkText automatically generates an alias for any empty internal links,
+// i.e. there's no text on the right side of the vertical bar, e.g. [[apple|]].
+export function updateLinkText(link: HTMLAnchorElement) {
+  const text = link.innerText;
+  const href = link.getAttr("href");
+
+  // Check for missing alias.
+  if (!text) {
+    const finalParen = /\s\(.*\)$/;
+    const finalComma = /,.*$/;
+
+    const hasFinalParenthesis = new RegExp(finalParen).test(href);
+
+    // Suppress final parenthesis if it exists, otherwise suppress final part
+    // beginning with a comma.
+    link.innerText = hasFinalParenthesis
+      ? href.replace(finalParen, "")
+      : href.replace(finalComma, "");
+
+    blendSuffix(link);
+  }
+}
+
+// blendSuffix moves any non-punctuation text that immediately follows the link
+// and adds it to the link text.
+function blendSuffix(link: HTMLElement) {
+  const sibling = link.nextSibling;
+
+  if (sibling.nodeType !== Node.TEXT_NODE) {
+    return;
+  }
+
+  const siblingText = sibling.nodeValue;
+
+  const idx = siblingText.indexOf(" ");
+
+  const suffix = idx > 0 ? siblingText.slice(0, idx) : "";
+
+  // If we found a non-punctuation suffix, move it from the sibling into the
+  // link text.
+  if (suffix.length > 0 && !punctuation.contains(suffix[0])) {
+    link.innerText += suffix;
+
+    sibling.nodeValue = siblingText.slice(suffix.length);
+  }
+}
+
+const punctuation = [
+  ".",
+  "?",
+  "!",
+  ",",
+  ";",
+  ":",
+  "-",
+  "–",
+  "—",
+  "[",
+  "]",
+  "(",
+  ")",
+  "{",
+  "}",
+  "'",
+  '"',
+  "…",
+];
